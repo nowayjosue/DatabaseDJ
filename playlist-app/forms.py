bd3d48f2c8028abb10f4adb9b12e5d3d3dd8a623
nowayjosue <115485393+nowayjosue@users.noreply.github.com>
2023-12-01 23:53:39 -0500

    first commit

---
+++ playlist-app/forms.py
@@ -0,0 +1,24 @@
+"""Forms for playlist app."""
+
+from wtforms import SelectField
+from flask_wtf import FlaskForm
+
+
+class PlaylistForm(FlaskForm):
+    """Form for adding playlists."""
+
+    name = StringField('Playlist Name', [validators.DataRequired()])
+
+
+class SongForm(FlaskForm):
+    """Form for adding songs."""
+
+    title = StringField('Song Title', [validators.DataRequired()])
+    artist = StringField('Artist', [validators.DataRequired()])
+
+
+# DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
+class NewSongForPlaylistForm(FlaskForm):
+    """Form for adding a song to playlist."""
+
+    song = SelectField('Song To Add', coerce=int)
